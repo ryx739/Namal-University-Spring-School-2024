5462eb3dc8516b48c1a37aca3ca2b4f6d6cc5866
Javed Ali <ryx739@gmail.com>
2024-04-24 19:13:19 +0500

    getting thread number

---
+++ omp/2 - thread number/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define NUM_THREDS 8
+
+int main(){
+    omp_set_num_threads(NUM_THREDS);
+
+    #pragma omp parallel
+        printf("Meow From Thread Num : %d\n", omp_get_thread_num());
+    return 0;
+}
